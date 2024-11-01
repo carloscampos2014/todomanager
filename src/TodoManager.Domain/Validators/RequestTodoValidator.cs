759387469503c8becf94761f1925001e8804f027
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2024-11-01 17:20:16 -0300

    feat: Validação de RequestTodoJson

---
+++ src/TodoManager.Domain/Validators/RequestTodoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using TodoManager.Domain.Contracts.Requests;
+
+namespace TodoManager.Domain.Validators;
+public class RequestTodoValidator : AbstractValidator<RequestTodoJson>
+{
+    public RequestTodoValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty().WithMessage("O nome é obrigatório.")
+            .Length(2, 100).WithMessage("O nome só pode ter de 2 a 100 caracteres.");
+
+        RuleFor(r => r.Description)
+            .NotEmpty().WithMessage("A descrição é obrigatório.")
+            .Length(3, 300).WithMessage("A descrição só pode ter de 3 a 300 caracteres.");
+    }
+
+    public override ValidationResult Validate(ValidationContext<RequestTodoJson> context)
+    {
+        if (context is null || context.InstanceToValidate is null)
+        {
+            return new ValidationResult(new List<ValidationFailure>() { new ValidationFailure(nameof(RequestTodoJson), "O objeto da lista não pode ser nulo.") });
+        }
+
+        return base.Validate(context);
+    }
+}
---
+++ tests/TodoManager.Domain.Tests/Faker/TodoFaker.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using TodoManager.Domain.Contracts.Enums;
+using TodoManager.Domain.Contracts.Requests;
+
+namespace TodoManager.Domain.Tests.Faker;
+
+public static class TodoFaker
+{
+    public static IEnumerable<RequestTodoJson> GenerateList(int cont)
+    {
+        var faker = new Faker<RequestTodoJson>()
+            .RuleFor(r => r.Name, f => f.Random.Words(3))
+            .RuleFor(r => r.Description, f => f.Random.Words(7))
+            .RuleFor(r => r.Deadline, f => f.Date.Recent())
+            .RuleFor(r => r.Priority, f => f.PickRandom<PriorityType>())
+            .RuleFor(r => r.Status, f => f.PickRandom<StatusType>());
+
+        return faker.Generate(cont);
+    }
+
+    public static RequestTodoJson GenerateObject()
+    {
+        return GenerateList(1).FirstOrDefault() ?? new RequestTodoJson();
+    }
+}
